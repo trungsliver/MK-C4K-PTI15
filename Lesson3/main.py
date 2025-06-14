965cd0cba2d732cfc9605cdceb4b56c3e68989b6
trungsliver <buiductrung.cva@gmail.com>
2025-06-14 15:17:11 +0700

    Lesson 3

--- Lesson3/main.py
+++ Lesson3/main.py
@@ -8,3 +8,18 @@ users_arr = [
 ]
 
 users_arr[0].show_info()
+
+# Test Database
+userDB = oop.UserDatabase()
+
+    # Độ dài 2 danh sách sau khởi tạo
+print('users_list:', len(userDB.users_list))
+print('users_dict:', len(userDB.users_dict))
+
+    # Lấy dữ liệu cho users_list
+userDB.convert_to_object()
+print('users_list:', len(userDB.users_list))
+print('users_dict:', len(userDB.users_dict))
+
+    # Hiển thị toàn bộ dữ liệu
+userDB.show_all()
