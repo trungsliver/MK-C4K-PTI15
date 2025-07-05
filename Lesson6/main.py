88c35d7c60cfa592b458626dc83e800817cd68e6
trungsliver <buiductrung.cva@gmail.com>
2025-07-05 15:00:37 +0700

    Lesson 6

---
+++ Lesson6/main.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6 import QtCore, QtWidgets
+from PyQt6 import uic
+import oop, data_io
+
+# xử lý
+app = QApplication(sys.argv)
+dtb = oop.UserDatabase()
+
+            
+# Hàm hiển thị thông báo
+def msg_box(title, content):
+    msg = QtWidgets.QMessageBox()
+    msg.setStyleSheet("QLabel{min-width: 200px;}"
+                          "QLabel{max-width: 200px;}"
+                          "QMessageBox{background-color:rgba(35,36,40,255);}"
+                          "QPushButton{background-color:rgb(30,95,181);}"
+                          "QLabel{color:rgb(255,255,255);}"
+                          "QPushButton{color:rgb(255,255,255);}")
+    msg.setWindowTitle(title)
+    msg.setInformativeText(content)
+    msg.exec()
+
+# Chuyển cửa sổ giao diện
+def switch_window(classw):
+    global window
+    window = classw
+    window.show()
+
+# Run app
+window = Signup()
+window.show()
+sys.exit(app.exec())
